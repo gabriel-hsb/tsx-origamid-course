47ee9ecc3cdd82ec45a875f76b70cdb1ae3c9f20
Gabriel <ghbranco6@gmail.com>
2024-10-31 20:50:17 -0300

    feat: toggle picture in picture

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Adicione funcionalidades ao player de vídeo:
 - [x] 1 - Use um estado reativo para verificar se o vídeo está tocando ou não.
 - [ ] 2 - Função para avançar o vídeo em +2s.
 - [ ] 3 - Função para alterar o playbackRate do vídeo.
-- [ ] 4 - Função para entrar/sair do modo pictureInPicture.
+- [x] 4 - Função para entrar/sair do modo pictureInPicture.
 - [ ] 5 - Função para alternar o som (mudo/não mudo) do vídeo.
--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { useRef, useState } from "react";
 import videoSrc from "./assets/video.mp4";
 
 function App() {
-  const [isVideoPlaying, setIsVideoPlaying] = useState<boolean>(false);
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const [isPictureInPicture, setIsPictureInPicture] = useState(false);
 
   const video = useRef<HTMLVideoElement>(null);
 
-  const playOrPauseVideo = () => {
+  const toggleVideoPlaying = () => {
     setIsVideoPlaying(!isVideoPlaying);
 
     if (isVideoPlaying) {
@@ -17,13 +18,32 @@ function App() {
     }
   };
 
+  const togglePictureInPicture = async () => {
+    setIsPictureInPicture(!isPictureInPicture);
+
+    if (isPictureInPicture) {
+      document.exitPictureInPicture();
+    } else {
+      try {
+        await video.current?.requestPictureInPicture();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
+
   return (
     <main>
       <h1>Hello tsx</h1>
       <div>
         <div className="flex">
-          <button onClick={playOrPauseVideo}>
+          <button onClick={toggleVideoPlaying}>
             {isVideoPlaying ? "Pause" : "Play"}
+          </button>
+          <button onClick={togglePictureInPicture}>
+            {isPictureInPicture
+              ? "Return to normal screen"
+              : "Turn on Picture in Picture"}
           </button>
         </div>
 
