d740a2399d76245b22dcbab9e2ae47e544856ee1
Gabriel <ghbranco6@gmail.com>
2024-10-31 20:31:56 -0300

    feat: play and pause video

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Adicione funcionalidades ao player de vídeo:
 
-- [ ] 1 - Use um estado reativo para verificar se o vídeo está tocando ou não.
+- [x] 1 - Use um estado reativo para verificar se o vídeo está tocando ou não.
 - [ ] 2 - Função para avançar o vídeo em +2s.
 - [ ] 3 - Função para alterar o playbackRate do vídeo.
 - [ ] 4 - Função para entrar/sair do modo pictureInPicture.
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import { useRef, useState } from "react";
+
+import videoSrc from "./assets/video.mp4";
+
 function App() {
+  const [isVideoPlaying, setIsVideoPlaying] = useState<boolean>(false);
+
+  const video = useRef<HTMLVideoElement>(null);
+
+  const playOrPauseVideo = () => {
+    setIsVideoPlaying(!isVideoPlaying);
+
+    if (isVideoPlaying) {
+      video.current?.pause();
+    } else {
+      video.current?.play();
+    }
+  };
 
   return (
-    <>
+    <main>
       <h1>Hello tsx</h1>
-    </>
-  )
+      <div>
+        <div className="flex">
+          <button onClick={playOrPauseVideo}>
+            {isVideoPlaying ? "Pause" : "Play"}
+          </button>
+        </div>
+
+        <video controls src={videoSrc} ref={video} />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
